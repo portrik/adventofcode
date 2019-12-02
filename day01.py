31dc3915eb846b79821c25e1d12e875c8798b92a
Patrik Dvořáček <dvorap74@fel.cvut.cz>
2019-12-02 20:01:32 +0100

    Day01

---
+++ day01.py
@@ -0,0 +1,21 @@
+import math
+
+def calculate_mass(inp_mass):
+    new_mass = math.floor(inp_mass / 3) - 2
+
+    if new_mass > 0:
+        new_mass += calculate_mass(new_mass)
+        return new_mass
+    
+    return 0
+
+if __name__ == "__main__":
+    total = 0
+
+    with open('./day01.txt') as file:
+        lines = file.readlines()
+
+    for line in lines:
+        total += calculate_mass(int(line))
+    
+    print(total)
