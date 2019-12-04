1fba632f9ee775a88ce01f2ce5a7f2ba12a0167e
Patrik Dvoracek <patrik.dvoracek@PRGA-000009.local>
2019-12-04 15:39:18 +0100

    Solution of part 1 of Day03

--- day03/day03.py
+++ day03/day03.py
@@ -15,6 +15,8 @@ class Graph:
         x_position = 0
         y_position = 0
 
+        self.vertexes.append(Vertex(x_position, y_position))
+
         for i in range(len(moves)):
             if moves[i][0] == "U":
                 y_position += int(moves[i][1:])
@@ -25,7 +27,25 @@ class Graph:
             elif moves[i][0] == "L":
                 x_position -= int(moves[i][1:])
 
-            self.vertexes.append(Vertex(x_position, y_position))   
+            self.vertexes.append(Vertex(x_position, y_position))
+
+def get_line(x1, x2, y1, y2):
+    line = []
+
+    if x2 < x1:
+        x1, x2 = x2, x1
+
+    if y2 < y1:
+        y1, y2 = y2, y1
+
+    if x1 == x2:
+        for k in range(y1, y2, 1):
+            line.append((x1, k))
+    else:
+        for k in range(x1, x2, 1):
+            line.append((k, y1))
+
+    return line
 
 if __name__ == '__main__':
     with open(os.path.dirname(__file__) + '/first_input.txt') as file:
@@ -39,45 +59,16 @@ if __name__ == '__main__':
 
     min_distance = math.inf
 
-    for i in range(len(first.vertexes) - 1):
-        for j in range(len(second.vertexes) - 1):
-            if first.vertexes[i].x_position == first.vertexes[i + 1].x_position and second.vertexes[j].y_position == second.vertexes[j + 1].y_position:
-                further = second.vertexes[j].x_position
-                closer = second.vertexes[j + 1].x_position
-                higher = first.vertexes[i].y_position
-                lower = first.vertexes[i + 1].y_position
+    for i in range(0, len(first.vertexes) - 1, 1):
+        print('Step %s out of %s' % (i, len(first.vertexes) - 2))
+        compare_line = get_line(first.vertexes[i].x_position, first.vertexes[i + 1].x_position, first.vertexes[i].y_position, first.vertexes[i + 1].y_position)
 
-                x_pos = first.vertexes[i].x_position
-                y_pos = second.vertexes[j].y_position
-
-                if further < closer:
-                    further, closer = closer, further
-                
-                if higher < lower:
-                    higher, lower = lower, higher
-
-                if (x_pos > closer and x_pos < further) and (y_pos > lower and y_pos < higher):
-                    if abs(x_pos) + abs(y_pos) < min_distance:
-                        min_distance = abs(x_pos) + abs(y_pos)
-
-            elif first.vertexes[i].y_position == first.vertexes[i + 1].y_position and second.vertexes[j].x_position == second.vertexes[j + 1].x_position:
-                further = second.vertexes[j].y_position
-                closer = second.vertexes[j + 1].y_position
-                higher = first.vertexes[i].x_position
-                lower = first.vertexes[i + 1].x_position
-
-                x_pos = first.vertexes[i].y_position
-                y_pos = second.vertexes[j].x_position
-
-                if further < closer:
-                    further, closer = closer, further
-                
-                if higher < lower:
-                    higher, lower = lower, higher
-
-                if (x_pos > closer and x_pos < further) and (y_pos > lower and y_pos < higher):
-                    if abs(x_pos) + abs(y_pos) < min_distance:
-                        min_distance = abs(x_pos) + abs(y_pos)
-
-
-    print(min_distance)
+        for j in range(0, len(second.vertexes) - 1, 1):
+            new_line = get_line(second.vertexes[j].x_position, second.vertexes[j + 1].x_position, second.vertexes[j].y_position, second.vertexes[j + 1].y_position)
+            for line in compare_line:
+                if line != (0, 0):
+                    if line in new_line:
+                        if abs(line[0]) + abs(line[1]) < min_distance:
+                            min_distance = abs(line[0]) + abs(line[1])
+    
+    print('Minimal distance is %s' % min_distance)
