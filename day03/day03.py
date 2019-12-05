82e9af8d42142ee626bcd068d077f0edd46a3dfe
Patrik Dvoracek <patrik.dvoracek@PRGA-000009.local>
2019-12-05 13:24:48 +0100

    Made Day03 much much better

--- day03/day03.py
+++ day03/day03.py
@@ -51,16 +51,7 @@ def get_line(x1, x2, y1, y2):
 
     return line
 
-if __name__ == '__main__':
-    with open(os.path.dirname(__file__) + '/first_input.txt') as file:
-        first = file.readlines()[0].split(',')
-    
-    with open(os.path.dirname(__file__) + '/second_input.txt') as file:
-        second = file.readlines()[0].split(',')
-
-    first = Graph(first)
-    second = Graph(second)
-
+def dumb_approach(first, second):
     min_distance = math.inf
     steps = math.inf
 
@@ -81,6 +72,74 @@ if __name__ == '__main__':
 
                         if steps_first + steps_second < steps:
                             steps = steps_first + steps_second
+    
+    return min_distance, steps
+
+def better_approach(first, second):
+    min_distance = math.inf
+    steps = math.inf
+
+    for i in range(0, len(first.vertexes) - 1, 1):
+        print('Step %s out of %s' % (i, len(first.vertexes) - 2))
+
+        x1 = first.vertexes[i].x_position
+        x2 = first.vertexes[i + 1].x_position
+        y1 = first.vertexes[i].y_position
+        y2 = first.vertexes[i + 1].y_position
+
+        if x1 > x2:
+            x1, x2 = x2, x1
+        
+        if y1 > y2:
+            y1, y2 = y2, y1
+        
+        for j in range(0, len(second.vertexes) - 1, 1):
+            x3 = second.vertexes[j].x_position
+            x4 = second.vertexes[j + 1].x_position
+            y3 = second.vertexes[j].y_position
+            y4 = second.vertexes[j + 1].y_position
+
+            if x3 > x4:
+                x3, x4 = x4, x4
+
+            if y3 > y4:
+                y3, y4 = y4, y3
+
+            if (x1 == x2) and (y3 == y4):
+                if ((x1 > x3) and (x1 < x4)) and ((y3 > y1) and (y3 < y4)):
+                    if abs(x1) + abs(y3) < min_distance:
+                        min_distance = abs(x1) + abs(y3)
+                    
+                    steps_first = first.vertexes[i].steps + abs(first.vertexes[i].x_position - x1) + abs(first.vertexes[i].y_position - y3)
+                    steps_second = second.vertexes[j].steps + abs(second.vertexes[j].x_position - x1) + abs(second.vertexes[j].y_position - y3)
+
+                    if steps_first + steps_second < steps:
+                        steps = steps_first + steps_second 
+
+            elif (x3 == x4) and (y1 == y2):
+                if ((x3 > x1) and (x3 < x2)) and ((y1 > y3) and (y1 < y4)):
+                    if abs(x3) + abs(y1) < min_distance:
+                        min_distance = abs(x3) + abs(y1)
+
+                        steps_first = first.vertexes[i].steps + abs(first.vertexes[i].x_position - x3) + abs(first.vertexes[i].y_position - y1)
+                        steps_second = second.vertexes[j].steps + abs(second.vertexes[j].x_position - x3) + abs(second.vertexes[j].y_position - y1)
+
+                        if steps_first + steps_second < steps:
+                            steps = steps_first + steps_second
+
+    return min_distance, steps
+
+if __name__ == '__main__':
+    with open(os.path.dirname(__file__) + '/first_input.txt') as file:
+        first = file.readlines()[0].split(',')
+    
+    with open(os.path.dirname(__file__) + '/second_input.txt') as file:
+        second = file.readlines()[0].split(',')
+
+    first = Graph(first)
+    second = Graph(second)
+
+    min_distance, steps = better_approach(first, second)
 
     print('Minimal distance is %s' % min_distance)
     print('Minimal steps needed is %s' % steps)
