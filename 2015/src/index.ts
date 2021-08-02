a59badb1cfe4fa88a7791d8f2059f47ac616f941
Patrik Dvořáček <patrik@dvoracek.dev>
2021-08-02 16:43:42 +0200

    Added better main handling

--- 2015/src/index.ts
+++ 2015/src/index.ts
@@ -1,11 +1,31 @@
 import { results01 } from './01';
 import { results02 } from './02';
 import { results03 } from './03';
-//import { results04 } from './04';
+import { results04 } from './04';
 import { results05 } from './05';
+import { results06 } from './06';
 
-results01();
-results02();
-results03();
-//results04();
-results05();
+const part = process.argv[2];
+
+switch (part) {
+case '01':
+	results01();
+	break;
+case '02':
+	results02();
+	break;
+case '03':
+	results03();
+	break;
+case '04':
+	results04();
+	break;
+case '05':
+	results05();
+	break;
+case '06':
+	results06();
+	break;
+default:
+	throw new Error(`"${part}" is not a recognized problem!`);
+}
