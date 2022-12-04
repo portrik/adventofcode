69a15142c8b55bf785e02eeb667fdd89bf7a60d9
Patrik Dvořáček <patrik@dvoracek.dev>
2022-12-04 11:50:17 +0100

    style(2022): :art: Simplify Day 03.
    Some updates from Day 04.

--- 2022/day_03/day_03.v
+++ 2022/day_03/day_03.v
@@ -10,14 +10,13 @@ fn add_char(char rune) int {
 
 pub fn solve_first(input string) int {
 	mut result := 0
-	rucksacks := input.split('\n')
+	rucksacks := input.split('\n').map(fn (rucksack string) [][]rune {
+		return [rucksack[..rucksack.len / 2].runes(), rucksack[rucksack.len / 2..].runes()]
+	})
 
 	for rucksack in rucksacks {
-		left := rucksack[..rucksack.len / 2].runes()
-		right := rucksack[rucksack.len / 2..].runes()
-
-		for value in left {
-			if right.index(value) >= 0 {
+		for value in rucksack[0] {
+			if rucksack[1].index(value) >= 0 {
 				result += add_char(value)
 				break
 			}
@@ -29,9 +28,7 @@ pub fn solve_first(input string) int {
 
 pub fn solve_second(input string) int {
 	mut result := 0
-	rucksacks := input.split('\n').map(fn (r string) []rune {
-		return r.runes()
-	})
+	rucksacks := input.split('\n').map(it.runes())
 
 	for i := 0; i < rucksacks.len; i += 3 {
 		for value in rucksacks[i] {
