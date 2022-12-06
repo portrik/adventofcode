b36df5a9c3e3faf437eb476478598750a7a8e9aa
Patrik Dvořáček <patrik@dvoracek.dev>
2022-12-06 23:45:24 +0100

    feat(2019): :sparkles: Add missing Day 02 test.

--- 2019/adventofcode/day_02/test_day_02.py
+++ 2019/adventofcode/day_02/test_day_02.py
@@ -1,8 +1,19 @@
 """Test Day 02."""
 
-from . import solve_first
+import os
+
+from . import solve_first, solve_second
 
 
 def test_day_02_solve_first():
     """Test the first solution."""
     assert solve_first("1,9,10,3,2,3,11,0,99,30,40,50") == 3500
+
+
+def test_day_02_solve_second():
+    """Test the second solution."""
+    input_path = os.path.join(os.path.dirname(__file__), "input.txt")
+    with open(input_path, encoding="UTF-8") as file:
+        day_input = file.read()
+
+    assert solve_second(day_input) == 9820
